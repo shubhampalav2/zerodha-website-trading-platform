b71b0bfa045fdb146bce4cce23cf086872baca8c
shubhampalav2 <palavshubham09@gmail.com>
2026-06-21 22:49:36 +0530

    designed 404 page

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,5 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import "./NotFound.css";
 export default function NotFound() {
-    return <>
-        <h2>404 Page Not Found</h2>
-    </>
+    const navigate = useNavigate();
+    const handleRedirect = () => {
+        navigate('/');
+    }
+    return <div className="notfound-page">
+        <div className="notfound-container">
+
+            {/* Error Code */}
+            <p className="error-code">404</p>
+
+            {/* Heading */}
+            <h1 className="title">
+                Kiaan couldn’t find that page
+            </h1>
+
+            {/* Description */}
+            <p className="description">
+                We couldn’t find the page you were looking for.
+            </p>
+
+            {/* CTA */}
+            <button
+                className="home-link"
+                onClick={handleRedirect}
+            >
+                Visit Zerodha's home page
+            </button>
+
+        </div>
+    </div>
 }
